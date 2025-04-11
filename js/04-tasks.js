f32231aa5b7796c971bf933f4e6fc11db6a9e7bb
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-04-11 09:15:47 +0300

    update

--- js/04-tasks.js
+++ js/04-tasks.js
@@ -1,51 +1,7 @@
-/**
- * Напиши функцію logItems(items), яка отримує масив та використовує цикл,
- * який для кожного елемента масиву буде виводити в консоль повідомлення у
- * форматі <номер елемента> - <значення елемента>. Нумерація елементів
- * повинна починатися з 1.
- *
- * Наприклад для першого елемента масиву ['Mango', 'Poly', 'Ajax'] з
- * індексом 0 буде виведено 1 - Mango, а для індексу 2 виведе 3 - Ajax.
- */
+// 1. Напиши функцію яка приймає два масиви (arr1, arr2), та повертає новий масив
+//    де будуть лише ті елементи які зустрічаються і в arr1 і arr2.
 
-function logItems(items) {}
+// 2. Напиши функцію яка приймає два масиви (arr1, arr2), та повертає новий масив
+//    де будуть лише елементи масиву arr1 яких не має у масиві arr2.
 
-logItems(["Mango", "Poly", "Ajax"]);
-logItems(["🍎", "🍇", "🍑", "🍌", "🍋"]);
-
-/**
- * Напиши функцію printInfo(names, phones) яка виводить
- * у консоль ім'я та телефонний номер користувача. У параметри
- * names та phones будуть передані рядки імен та телефонних номерів,
- * розділені комами. Порядковий номер імен та телефонів у рядках
- * вказують на відповідність. Кількість імен та телефонів
- * гарантовано однакова.
- */
-
-function printInfo(names, phones) {}
-
-printInfo(
-  "Jacob,William,Solomon,Artemis",
-  "89001234567,89001112233,890055566377,890055566300"
-);
-
-/**
- * Напиши функцію formatTime(minutes) яка переведе значення
- * minutes (кількість хвилин) у рядок у форматі годин
- * та хвилин HH:MM.
- */
-
-const hours = Math.floor(totalMinutes / 60);
-const minutes = totalMinutes % 60;
-console.log(hours);
-console.log(minutes);
-
-const doubleDigitHours = String(hours).padStart(2, 0);
-const doubleDigitMinutes = String(minutes).padStart(2, 0);
-console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
-
-function formatTime(minutes) {}
-
-console.log(formatTime(70)); // "01:10"
-console.log(formatTime(450)); // "07:30"
-console.log(formatTime(1441)); // "24:01"
+// 3. Напиши функцію яка приймає масив та знаходить мінімальний елемент.
